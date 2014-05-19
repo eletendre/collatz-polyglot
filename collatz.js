1395894e11aafe03d9bb34e1dc443071a99fc11a
eletendre <eletendre15@smpanthers.org>
2014-05-19 13:47:54 -0700

    collatz

---
+++ collatz.js
@@ -0,0 +1,17 @@
+function collatz(x) {
+	var seq  = 'x';
+	
+	while (x > 1) {
+		if (x % 2 == 0:) {
+			x = x / 2;
+		}
+		else {
+			x = 3*x + 1;
+		}
+		seq.append(x);
+	}
+	return seq
+}
+
+var result = collatz(31);
+console.log(result);
